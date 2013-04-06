16a983a560b109f7efb3cca34f817eb05c9f1674
TheImplementer <none@none>
2013-04-06 23:25:46 +0200

    [dev] usign InstructionDecoderSelector in instruction parser

--- src/main/java/org/bitbucket/theimplementer/mipsdisassembler/ApplicationConfiguration.java
+++ src/main/java/org/bitbucket/theimplementer/mipsdisassembler/ApplicationConfiguration.java
@@ -3,6 +3,8 @@ package org.bitbucket.theimplementer.mipsdisassembler;
 import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
 import net.emaze.dysfunctional.dispatching.delegates.Delegate;
 import org.bitbucket.theimplementer.mipsdisassembler.instructions.Instruction;
+import org.bitbucket.theimplementer.mipsdisassembler.instructions.InstructionDecoder;
+import org.bitbucket.theimplementer.mipsdisassembler.instructions.InstructionDecoderSelector;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,9 +13,13 @@ import java.util.List;
 @Configuration
 public class ApplicationConfiguration {
 
+    public Delegate<InstructionDecoder, Integer> instructionDecoderSelector(List<InstructionDecoder> instructionDecoders) {
+        return new InstructionDecoderSelector(instructionDecoders);
+    }
+
     @Bean
-    public Delegate<Instruction, Integer> instructionParser() {
-        return new DefaultInstructionParser();
+    public Delegate<Instruction, Integer> instructionParser(Delegate<InstructionDecoder, Integer> instructionDecoderSelector) {
+        return new DefaultInstructionParser(instructionDecoderSelector);
     }
 
     @Bean
--- src/main/java/org/bitbucket/theimplementer/mipsdisassembler/DefaultInstructionParser.java
+++ src/main/java/org/bitbucket/theimplementer/mipsdisassembler/DefaultInstructionParser.java
@@ -1,12 +1,22 @@
 package org.bitbucket.theimplementer.mipsdisassembler;
 
+import net.emaze.dysfunctional.contracts.dbc;
 import net.emaze.dysfunctional.dispatching.delegates.Delegate;
 import org.bitbucket.theimplementer.mipsdisassembler.instructions.Instruction;
+import org.bitbucket.theimplementer.mipsdisassembler.instructions.InstructionDecoder;
 
 public class DefaultInstructionParser implements Delegate<Instruction, Integer> {
 
+    private final Delegate<InstructionDecoder, Integer> instructionDecoderSelector;
+
+    public DefaultInstructionParser(Delegate<InstructionDecoder, Integer> instructionDecoderSelector) {
+        dbc.precondition(instructionDecoderSelector != null, "instructionDecoderSelector cannot be null");
+        this.instructionDecoderSelector = instructionDecoderSelector;
+    }
+
     @Override
-    public Instruction perform(Integer integer) {
-        return null;
+    public Instruction perform(Integer instructionAsInteger) {
+        final InstructionDecoder decoder = instructionDecoderSelector.perform(instructionAsInteger);
+        return decoder.perform(instructionAsInteger);
     }
 }
