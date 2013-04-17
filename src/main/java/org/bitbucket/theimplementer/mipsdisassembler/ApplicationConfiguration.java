02f7794707b014dd9b3cc49fcd22d3677c9860ec
TheImplementer <none@none>
2013-04-18 01:23:05 +0200

    [dev] configuration of instruction decoders for r-type and i-type instructions

--- src/main/java/org/bitbucket/theimplementer/mipsdisassembler/ApplicationConfiguration.java
+++ src/main/java/org/bitbucket/theimplementer/mipsdisassembler/ApplicationConfiguration.java
@@ -1,10 +1,7 @@
 package org.bitbucket.theimplementer.mipsdisassembler;
 
 import net.emaze.dysfunctional.dispatching.delegates.Delegate;
-import org.bitbucket.theimplementer.mipsdisassembler.instructions.Instruction;
-import org.bitbucket.theimplementer.mipsdisassembler.instructions.InstructionDecoder;
-import org.bitbucket.theimplementer.mipsdisassembler.instructions.InstructionDecoderSelector;
-import org.bitbucket.theimplementer.mipsdisassembler.instructions.JTypeInstructionDecoder;
+import org.bitbucket.theimplementer.mipsdisassembler.instructions.*;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,6 +15,16 @@ public class ApplicationConfiguration {
     @Bean
     public InstructionDecoder jTypeInstructionDecoder() {
         return new JTypeInstructionDecoder();
+    }
+
+    @Bean
+    public InstructionDecoder rTypeInstructionDecoder() {
+        return new RTypeInstructionDecoder();
+    }
+
+    @Bean
+    public InstructionDecoder iTypeInstructionDecoder() {
+        return new ITypeInstructionDecoder();
     }
 
     @Bean
