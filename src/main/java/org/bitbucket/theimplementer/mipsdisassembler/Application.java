0a8bfca98b937b3463b11b91670e37ef05b07997
TheImplementer <none@none>
2013-04-19 00:26:09 +0200

    [ref] introduced gui

--- src/main/java/org/bitbucket/theimplementer/mipsdisassembler/Application.java
+++ src/main/java/org/bitbucket/theimplementer/mipsdisassembler/Application.java
@@ -1,30 +1,11 @@
 package org.bitbucket.theimplementer.mipsdisassembler;
 
-import net.emaze.dysfunctional.Applications;
-import net.emaze.dysfunctional.Casts;
-import net.emaze.dysfunctional.dispatching.delegates.Delegate;
-import org.bitbucket.theimplementer.mipsdisassembler.instructions.Instruction;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Iterator;
-import java.util.List;
-import net.emaze.dysfunctional.tuples.Pair;
-import net.emaze.dysfunctional.tuples.PairSecond;
+import org.bitbucket.theimplementer.mipsdisassembler.gui.MainFrame;
 
 public class Application {
 
-    public static void main(String[] args) throws FileNotFoundException, IOException {
-        final FileInputStream stream = new FileInputStream("SLUS_011.56");
-        stream.skip(1024);
-        final AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfiguration.class);
-        final Delegate<List<Pair<Integer, Instruction>>, InputStream> mipsDisassembler = Casts.widen(context.getBean("mipsDisassembler"));
-        final List<Pair<Integer, Instruction>> opcodesAndInstructions = mipsDisassembler.perform(stream);
-        final Iterator<Instruction> instructions = Applications.transform(opcodesAndInstructions, new PairSecond<Integer, Instruction>());
-        Applications.each(instructions, new PrintInstruction());
+    public static void main(String[] args) {
+        final MainFrame mainFrame = new MainFrame();
+        mainFrame.setVisible(true);
     }
-
 }
---
+++ src/main/java/org/bitbucket/theimplementer/mipsdisassembler/gui/MainFrame.java
@@ -0,0 +1,113 @@
+package org.bitbucket.theimplementer.mipsdisassembler.gui;
+
+import net.emaze.dysfunctional.Casts;
+import net.emaze.dysfunctional.dispatching.delegates.Delegate;
+import net.emaze.dysfunctional.tuples.Pair;
+import org.bitbucket.theimplementer.mipsdisassembler.ApplicationConfiguration;
+import org.bitbucket.theimplementer.mipsdisassembler.instructions.Instruction;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.List;
+
+public class MainFrame extends JFrame {
+
+    private JMenuBar menuBar;
+    private JMenu fileMenu;
+    private JMenu helpMenu;
+    private JMenuItem openMenuItem;
+    private JMenuItem exitMenuItem;
+    private JMenuItem aboutMenuItem;
+    private JSeparator fileMenuSeparator;
+    private JTable contentTable;
+
+    final AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfiguration.class);
+
+    public MainFrame() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        setTitle("Mips Disassembler");
+
+        menuBar = new JMenuBar();
+
+        fileMenu = new JMenu("File");
+        openMenuItem = new JMenuItem("Open");
+        openMenuItem.addActionListener(new OpenFileActionListener(this));
+        fileMenuSeparator = new JSeparator();
+        exitMenuItem = new JMenuItem("Exit");
+        fileMenu.add(openMenuItem);
+        fileMenu.add(fileMenuSeparator);
+        fileMenu.add(exitMenuItem);
+        menuBar.add(fileMenu);
+
+        helpMenu = new JMenu("?");
+        aboutMenuItem = new JMenuItem("About");
+        helpMenu.add(aboutMenuItem);
+        menuBar.add(helpMenu);
+
+        setJMenuBar(menuBar);
+
+        setLayout(new BorderLayout());
+
+        contentTable = new JTable();
+        contentTable.setPreferredScrollableViewportSize(new Dimension(300, 50));
+        contentTable.setFillsViewportHeight(true);
+        final JScrollPane scrollPane = new JScrollPane(contentTable);
+        add(scrollPane);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        setSize(800, 600);
+    }
+
+    public void loadFile(File file) throws FileNotFoundException {
+        final Delegate<List<Pair<Integer, Instruction>>, InputStream> mipsDisassembler = Casts.widen(context.getBean("mipsDisassembler"));
+        final List<Pair<Integer, Instruction>> opcodesAndInstructions = mipsDisassembler.perform(new FileInputStream(file));
+        updateContent(opcodesAndInstructions);
+    }
+
+    private void updateContent(java.util.List<Pair<Integer, Instruction>> opcodesAndInstructions) {
+        final Object[][] newContent = new Object[opcodesAndInstructions.size()][2];
+        for (int counter = 0; counter < opcodesAndInstructions.size(); counter++) {
+            final Pair<Integer, Instruction> opcodeAndInstruction = opcodesAndInstructions.get(counter);
+            newContent[counter][0] = String.format("%08X", opcodeAndInstruction.first().intValue());
+            newContent[counter][1] = opcodeAndInstruction.second();
+        }
+        contentTable.setModel(new DefaultTableModel(newContent, new String[]{"Opcode", "Instruction"}));
+        contentTable.getColumnModel().getColumn(0).setMaxWidth(100);
+        contentTable.getColumnModel().getColumn(0).setCellEditor(null);
+    }
+
+    public static class OpenFileActionListener implements ActionListener {
+
+        private final MainFrame parent;
+
+        public OpenFileActionListener(MainFrame parent) {
+            this.parent = parent;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent event) {
+            final JFileChooser openFileChooser = new JFileChooser();
+            final int userOpenAction = openFileChooser.showOpenDialog(parent);
+            if (userOpenAction == JFileChooser.APPROVE_OPTION) {
+                final File selectedFile = openFileChooser.getSelectedFile();
+                try {
+                    parent.loadFile(selectedFile);
+                } catch (FileNotFoundException ex) {
+                    JOptionPane.showMessageDialog(parent, "Cannot find selected file.");
+                }
+            }
+        }
+    }
+}
