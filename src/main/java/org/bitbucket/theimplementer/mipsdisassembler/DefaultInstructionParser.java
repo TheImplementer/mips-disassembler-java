1442e7a1df00e00149b2e870a583f62017fc28a4
TheImplementer <none@none>
2013-04-18 14:13:50 +0200

    [fix] handling unsupported instructions

--- src/main/java/org/bitbucket/theimplementer/mipsdisassembler/DefaultInstructionParser.java
+++ src/main/java/org/bitbucket/theimplementer/mipsdisassembler/DefaultInstructionParser.java
@@ -4,6 +4,9 @@ import net.emaze.dysfunctional.contracts.dbc;
 import net.emaze.dysfunctional.dispatching.delegates.Delegate;
 import org.bitbucket.theimplementer.mipsdisassembler.instructions.Instruction;
 import org.bitbucket.theimplementer.mipsdisassembler.instructions.InstructionDecoder;
+import org.bitbucket.theimplementer.mipsdisassembler.instructions.NoInstructionDecoderFoundError;
+import org.bitbucket.theimplementer.mipsdisassembler.instructions.UnknownInstruction;
+import org.bitbucket.theimplementer.mipsdisassembler.instructions.UnknownInstructionException;
 
 public class DefaultInstructionParser implements Delegate<Instruction, Integer> {
 
@@ -16,16 +19,16 @@ public class DefaultInstructionParser implements Delegate<Instruction, Integer> 
 
     @Override
     public Instruction perform(Integer instructionAsInteger) {
-        final InstructionDecoder decoder = instructionDecoderSelector.perform(instructionAsInteger);
+        InstructionDecoder decoder;
+        try {
+            decoder = instructionDecoderSelector.perform(instructionAsInteger);
+        } catch (NoInstructionDecoderFoundError ex) {
+            return new UnknownInstruction();
+        }
         try {
             return decoder.perform(instructionAsInteger);
-        } catch (Exception ex) {
-            return new Instruction() {
-                @Override
-                public String toString() {
-                    return "Unknown instruction";
-                }
-            };
+        } catch (UnknownInstructionException ex) {
+            return new UnknownInstruction();
         }
     }
 }
---
+++ src/main/java/org/bitbucket/theimplementer/mipsdisassembler/instructions/UnknownInstruction.java
@@ -0,0 +1,10 @@
+package org.bitbucket.theimplementer.mipsdisassembler.instructions;
+
+
+public class UnknownInstruction implements Instruction {
+
+    @Override
+    public String toString() {
+        return "Unknown instruction";
+    }
+}
